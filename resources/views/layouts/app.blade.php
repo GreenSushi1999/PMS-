ee5f0bb52841f4fc68d1ad9591ff1fa2161847b9
Rodel Santos <rodelsantos1999@gmail.com>
2023-12-06 19:24:09 +0800

    new branch

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'My Laravel App')</title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <header>
+        <!-- Header content -->
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <!-- Footer content -->
+    </footer>
+
+    <!-- Add your Bootstrap JavaScript link here if needed -->
+</body>
+
+</html>
