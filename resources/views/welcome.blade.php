def8de2215b06292046fa6b57bbfc3d3aedf36a6
GreenSushi1999 <rodelsantos1999@gmail.com>
2023-12-07 14:13:55 +0800

    save

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,19 +11,19 @@
     </style>
     <div class="container">
         <div class="row d-flex justify-content-center align-items-center mt-5">
-            <div class="col-lg-5">
+            <div class="col-lg-4">
                 <div class="card m-1 shadow rounded">
                     <div class="card-title text-center mt-3 p-4">
-                        <img src="/img/logo.png" alt="" height="300;" width="300;">
+                        <img src="/img/logo.png" alt="" height="100;" width="100;">
                         <h1 class="card-title text-success">MTC Performance Management System</h1>
                     </div>
                     <div class="card-body bg-white p-5 m-1">
                         <div class='row justify-content-center'>
-                            <button class="btn btn-primary col-lg-10 btn-lg" style="font-size:22px;">
+                            <button class="btn btn-primary col-lg-10">
                                 Ratee</button>
                         </div>
                         <div class='row justify-content-center mt-2'>
-                            <button class="btn btn-danger col-lg-10 btn-lg">Rater</button>
+                            <button class="btn btn-danger col-lg-10">Rater</button>
                         </div>
                     </div>
                 </div>
